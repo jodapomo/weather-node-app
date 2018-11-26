49ee049da44081af4dbe409d9eeba5d9e047596a
José Posada <josedanielpm@gmail.com>
2018-11-25 23:55:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather Console APP
+
+## Run app
+
+```
+    npm install
+```
+
+Temperature of a place:
+```
+    node app -d "Medellin Colombia"
+```
---
+++ lugar/lugar.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+
+const getLugarLatLng = async ( direccion) => {
+
+    let encodedUrl = encodeURI( direccion );
+
+    let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyBkDYSVRVtQ6P2mf2Xrq0VBjps8GEcWsLU`);
+
+    if ( resp.data.status == 'ZERO_RESULTS' ) {
+        throw new Error(`No hay resultados para la ciudad ${ direccion }`);
+    }
+
+    let location = resp.data.results[0];
+    let coors = location.geometry.location;
+
+
+
+
+
+
+    return {
+        direccion: location.formatted_address,
+        lat: coors.lat,
+        lng: coors.lng
+    }
+
+}
+
+
+module.exports = {
+    getLugarLatLng
+}
